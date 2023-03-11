a5ed4dd8dea8d1c079724c225ec41c26a81047fe
Leon Zuev <loonatune@gmail.com>
2023-03-11 23:34:59 +0200

    Add parsePet method, bug setters.

--- src/homePets/Pet.java
+++ src/homePets/Pet.java
@@ -27,18 +27,20 @@ public class Pet {
   private String date = null;
   private Double weight = 0.0;
 
+  //вопрос по сеттерам
+
   public setName(String name) {
-    this.name = petName;
+    this.name = "petName";
   }
   private setDate(String date) { //дата рождения неизменна, поэтому приват?
-    this.date = petDate;
+    this.date = "petDate";
   }
   public void setWeight(Double weight) {
     if (weight < 0){
       System.out.println("Вес не может быть отрицательным: " + weight);
       return;
     }
-    this.weight = petWeight;
+    this.weight = 5.5;
   }
 
   public String getName() {
@@ -49,6 +51,20 @@ public class Pet {
   }
   public int getWeight() {
     return weight;
+  }
+  /*
+  статический метод для создания животного при прочтении данных из строки
+  "dog,кличка", "cat,кличка,вес", "turtle,кличка,вес,дата рождения".
+   */
+  final private static char SEP = ',';
+  public static Pet parsePet(String name, String date, int weight) {
+    int sepIndex = line.indexOf(SEP);
+    if (sepIndex != -1) {
+      String name = line.substring(0, sepIndex);
+      String date - line.substring(sepIndex+1, line.lastIndexOf(SEP));
+
+    }
+
   }
 
 
